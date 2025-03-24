3887b8d2d9ca04b69aa9833e59dc86ac7e4b371c
Kelvin Chappell <3847714+kchapl@users.noreply.github.com>
2025-03-24 21:58:09 +0000

    chore: apply formatting

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,4 +1,5 @@
 // For more information on writing tests, see
+
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
