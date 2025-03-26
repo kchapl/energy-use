f7cd4e687d7ada2478b4f7dca49ec3b0e55437b6
Kelvin Chappell <3847714+kchapl@users.noreply.github.com>
2025-03-26 21:28:16 +0000

    feat: update main function to read and calculate energy usage

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+package energyuse
 
-def msg = "I was compiled by Scala 3. :)"
+import java.time.Instant
+
+@main def run(args: String*): Unit =
+  if (args.length == 3) {
+    val readings = Calculations.readEnergyData(args(0))
+    val start    = Instant.parse(args(1))
+    val end      = Instant.parse(args(2))
+    val usage    = Calculations.calculateEnergyUsage(readings, start, end)
+    println(usage)
+  }
